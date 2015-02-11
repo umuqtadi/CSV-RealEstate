a79aab6f36d7f0755614a07732cfb479c309e9e7
Dustin Kraft <kraftdr@gmail.com>
2015-02-11 12:04:47 -0700

    ch ch changes

--- CSV-RealEstate/Program.cs
+++ CSV-RealEstate/Program.cs
@@ -6,35 +6,90 @@ using System.Threading.Tasks;
 
 namespace CSV_RealEstate
 {
+    // WHERE TO START?
+    // 1. Complete the RealEstateType enumeration
+    // 2. Complete the RealEstateSale object.  Fill in all properties, then create the constructor.
+    // 3. Complete the GetRealEstateSaleList() function.  This is the function that actually reads in the .csv document and extracts a single row from the document and passes it into the RealEstateSale constructor to create a list of RealEstateSale Objects.
+    // 4. Start by displaying the the information in the Main() function by creating lambda expressions.  After you have acheived your desired output, then translate your logic into the function for testing.
     class Program
     {
         static void Main(string[] args)
         {
-            List<RealEstateData> realEstateDataList = new List<RealEstateData>();
-            //read in the realestatedata.csv file.  As you process each row, you'll add a new 
-            // RealEstateData object to the list for each row of the document, excluding the first.
-
-
+            List<RealEstateSale> realEstateSaleList = GetRealEstateSaleList();
+            
             //Display the average square footage of a Condo sold in the city of Sacramento, 
-            // round to 2 decimal points
+            //Use the GetAverageSquareFootageByRealEstateTypeAndCity() function.
 
-            //Display the total sales of all residential homes in Elk Grove, display in dollars
+            //Display the total sales of all residential homes in Elk Grove.  Use the GetTotalSalesByRealEstateTypeAndCity() function for testing.
 
-            //Display the total number of residential homes sold in the following  
-            // zip codes: 95842, 95825, 95815
+            //Display the total number of residential homes sold in the zip code 95842.  Use the GetNumberOfSalesByRealEstateTypeAndZip() function for testing.
 
-            //Display the average sale price of a lot in Sacramento, display in dollars
+            //Display the average sale price of a lot in Sacramento.  Use the GetAverageSalePriceByRealEstateTypeAndCity() function for testing.
 
-            //Display the average price per square foot for a condo in Sacramento, display in dollars
+            //Display the average price per square foot for a condo in Sacramento. Round to 2 decimal places. Use the GetAveragePricePerSquareFootByRealEstateTypeAndCity() function for testing.
 
-            //Display the number of all sales that were completed on a Wednesday
+            //Display the number of all sales that were completed on a Wednesday.  Use the GetNumberOfSalesByDayOfWeek() function for testing.
+
 
             //Display the average number of bedrooms for a residential home in Sacramento when the 
-            // price is greater than 300000, round to 2 decimal points
-            
-            //Extra Credit:
-            //Display top 5 cities and the number of homes sold (using the GroupBy extension)
+            // price is greater than 300000.  Round to 2 decimal places.  Use the GetAverageBedsByRealEstateTypeAndCityHigherThanPrice() function for testing.
 
+            //Extra Credit:
+            //Display top 5 cities by the number of homes sold (using the GroupBy extension)
+            // Use the GetTop5CitiesByNumberOfHomesSold() function for testing.
+
+        }
+
+        public static List<RealEstateSale> GetRealEstateSaleList()
+        {
+         
+            //read in the realestatedata.csv file.  As you process each row, you'll add a new 
+            // RealEstateData object to the list for each row of the document, excluding the first.  bool skipFirstLine = true;
+         
+            return new List<RealEstateSale>();
+        }
+
+        public static double GetAverageSquareFootageByRealEstateTypeAndCity(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city) 
+        {
+            return 0.0;
+        }
+
+        public static decimal GetTotalSalesByRealEstateTypeAndCity(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city)
+        {
+            return 0.0m;
+        }
+
+        public static int GetNumberOfSalesByRealEstateTypeAndZip(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string zipcode)
+        {
+            return 0;
+        }
+
+        
+        public static decimal GetAverageSalePriceByRealEstateTypeAndCity(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city)
+        {
+            //Must round to 2 decimal points
+            return 0.0m;
+        }
+        public static decimal GetAveragePricePerSquareFootByRealEstateTypeAndCity(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city)
+        {
+            //Must round to 2 decimal points
+            return 0.0m;
+        }
+
+        public static int GetNumberOfSalesByDayOfWeek(List<RealEstateSale> realEstateDataList, DayOfWeek dayOfWeek)
+        {
+            return 0;
+        }
+
+        public static double GetAverageBedsByRealEstateTypeAndCityHigherThanPrice(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city, decimal price)
+        {
+            //Must round to 2 decimal points
+            return 0.0;
+        }
+
+        public static List<string> GetTop5CitiesByNumberOfHomesSold(List<RealEstateSale> realEstateDataList)
+        {
+            return new List<string>();
         }
     }
 
@@ -42,8 +97,7 @@ namespace CSV_RealEstate
     {
         //fill in with enum types: Residential, MultiFamily, Condo, Lot
     }
-
-    public class RealEstateData
+    class RealEstateSale
     {
         //Create properties, using the correct data types (not all are strings) for all columns of the CSV
 
---
+++ CSV-RealEstate/Test.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSV_RealEstate
+{
+    [TestFixture]
+    class Test
+    {
+        public List<RealEstateSale> testData = Program.GetRealEstateSaleList();
+            //Display the average square footage of a Condo sold in the city of Sacramento, 
+            //Use the GetAverageSquareFootageByRealEstateTypeAndCity() function.
+        [Test]
+        public void GetAverageSquareFootageByRealEstateTypeAndCityTest()
+        {
+            var result = Program.GetAverageSquareFootageByRealEstateTypeAndCity(testData, RealEstateType.Condo, "sacramento");
+            Assert.IsTrue(result == 941.36, "Incorrect value");
+        }
+            //Display the total sales of all residential homes in Elk Grove.  Use the GetTotalSalesByRealEstateTypeAndCity() function.
+        [Test]
+        public void GetTotalSalesByRealEstateTypeAndCityTest()
+        {
+            var result = Program.GetTotalSalesByRealEstateTypeAndCity(testData, RealEstateType.Residential, "elk grove");
+            Assert.IsTrue(result == 28372477, "Incorrect value");
+        }
+
+        //Display the total number of residential homes sold in the zip code 95842.  Use the GetNumberOfSalesByRealEstateTypeAndZip() function.
+        [Test]
+        public void GetNumberOfSalesByRealEstateTypeAndZipTest()
+        {
+            var result = Program.GetNumberOfSalesByRealEstateTypeAndZip(testData, RealEstateType.Residential, "95842");
+            Assert.IsTrue(result == 16, "Incorrect value");
+        }
+
+        //Display the average sale price of a lot in Sacramento.  Use the GetAverageSalePriceByRealEstateTypeAndCity() function.
+        [Test]
+        public void GetAverageSalePriceByRealEstateTypeAndCityTest()
+        {
+            var result = Program.GetAverageSalePriceByRealEstateTypeAndCity(testData, RealEstateType.Lot, "sacramento");
+            Assert.IsTrue(result == 287483.67m, "Incorrect value");
+        }
+            //Display the average price per square foot for a condo in Sacramento.  Use the GetPricePerSquareFootByRealEstateTypeAndCity() function.
+        [Test]
+        public void GetAveragePricePerSquareFootByRealEstateTypeAndCityTest()
+        {
+            var result = Program.GetAveragePricePerSquareFootByRealEstateTypeAndCity(testData, RealEstateType.Condo, "sacramento");
+            Assert.IsTrue(result == 132.90m, "Incorrect value");
+        }
+
+        //Display the number of all sales that were completed on a Wednesday.  Use the GetNumberOfSalesByDayOfWeek() function.
+        [Test]
+        public void GetNumberOfSalesByDayOfWeekTest()
+        {
+            var result = Program.GetNumberOfSalesByDayOfWeek(testData, DayOfWeek.Wednesday);
+            Assert.IsTrue(result == 158, "Incorrect value");
+        }
+
+        //Display the average number of bedrooms for a residential home in Sacramento when the 
+        // price is greater than 300000.  Use the GetAverageBedsByRealEstateTypeAndCityHigherThanPrice() function.
+        [Test]
+        public void GetAverageBedsByRealEstateTypeAndCityHigherThanPriceTest()
+        {
+            var result = Program.GetAverageBedsByRealEstateTypeAndCityHigherThanPrice(testData, RealEstateType.Residential, "sacramento", 300000m);
+            Assert.IsTrue(result == 3.62, "Incorrect value");
+        }
+
+        //Extra Credit:
+        //Display top 5 cities and the number of homes sold (using the GroupBy extension)
+        // Use the GetTop5CitiesByNumberOfHomesSold() function.
+        [Test]
+        public void GetTop5CitiesByNumberOfHomesSoldTest()
+        {
+            List<string> results = Program.GetTop5CitiesByNumberOfHomesSold(testData);
+            Assert.IsTrue(results.Count == 5, "Incorrect number of results, should be 5");
+            Assert.IsTrue(results[0].ToLower() == "sacramento", "Top city incorrect");
+            Assert.IsTrue(results[2].ToLower() == "lincoln", "3rd city incorrect");
+            Assert.IsTrue(results[4].ToLower() == "citrus heights", "5th city incorrect");
+
+        }
+    }
+}
